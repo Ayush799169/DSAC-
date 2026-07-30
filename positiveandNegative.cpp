721322f7f5bc2fc0c36baca7739d9540a5306962
Ayush kumar yadav <ay4708504@gmail.com>
2026-07-30 07:50:04 +0530

    Eqals program

--- positiveandNegative.cpp
+++ positiveandNegative.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+/* #include<iostream>
 using namespace std;
 
 int main(){
@@ -16,6 +16,21 @@ int main(){
         cout<<a<<"Zero";
     }
     return 0;
-} 
+}  */
+
+#include<iostream>
+using namespace std;
+ int main () {
+    int a=20;
+
+    if(a==10){              //a==10 not ,20 eqal hoga 
+        cout<<"Equal A 20" <<endl;
+    }
+    else{
+        cout<<"Not eqal 5";
+    }
+ 
+ return 0;
+}
 
 
